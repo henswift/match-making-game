3e88959e1c9954b47db54595ca886c7c7f841d7a
Hen Weaver <henweaver@icloud.com>
2023-10-25 20:54:44 -0400

    Adding the initial files

---
+++ index.js
@@ -0,0 +1,25 @@
+/* The Match Making Game
+
+Do a 4x4 grid = 16 cards = 8 matches
+
+Click two cards at a time and it should tell you if they are matched
+
+If they are a match then they should disappear and the player gets the point
+
+    The image should just change from the front of a card to the back of the card
+
+If it's not a match the cards remain and they try again. 
+
+    If you pick the wrong card you lose a life. 
+    
+    If you pick the right ones you gain a life. 
+
+
+Two people playing the game and they take turns guessing. 
+
+
+
+
+
+
+*/
